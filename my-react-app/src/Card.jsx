4ed29c31deafe4280917b68e424532e442ced887
Dilip Waghmare <dilipwaghmarep15@gmail.com>
2024-03-08 17:49:05 +0530

    React Added
    React Added

---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,12 @@
+function Card()
+{
+    return(
+       <div className="card">
+          <img></img>
+          <h2>Dilip Waghmare</h2>
+          <p>I am Laravel Developer in Perfect Skills i deveping web applications currently i'm working on Talent Management Web-App project</p>
+       </div>
+    );
+}
+
+export default Card
---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer()
+{
+    return(
+    <footer><p>&copy; {new Date().getFullYear()} All rights are Reserved by TechBrainiacs</p></footer>
+    );
+}
+
+export default Footer
